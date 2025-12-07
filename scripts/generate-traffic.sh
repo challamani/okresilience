7ba0d70fdb16166bb1cc867a5e02fdff0d2191ef
challamani <mani.challa1990@gmail.com>
2025-12-07 22:08:10 +0000

    gateway and workload ratelimit resources

--- scripts/generate-traffic.sh
+++ scripts/generate-traffic.sh
@@ -4,8 +4,9 @@
 set -e
 export INGRESS_IP=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-for i in {1..50}; 
+for i in {1..31}; 
     do echo -e "\nRequest ==> [$i]"; 
-    curl -s -D - -o /dev/null -H "Host: httpbin.local" http://$INGRESS_IP/get; 
-    sleep 1; 
+    curl -s -D - -o /dev/null -H "Host: httpbin.local" http://$INGRESS_IP/status/200;
+    #curl -H "Host: httpbin.local" http://$INGRESS_IP/get; 
+    sleep 1;
 done
